b4629ae98dd097e8b6a4e786ffe012c0a1479b09
CodeAllen <allen5g@qq.com>
2021-02-01 21:45:00 +0800

    add some modify

--- 程序员面试笔记（C++）/003arithmetic.cpp
+++ 程序员面试笔记（C++）/003arithmetic.cpp
@@ -1,3 +1,34 @@
 #include <iostream>
+#include <cstdio>
 
 using namespace std;
+
+int add(int a, int b) {
+    return (a + b);
+}
+
+int minu(int a, int b) {
+    return (a - b);
+}
+
+int multi(int a, int b) {
+    return (a * b);
+}
+
+int divide(int a, int b) {
+    return (a / b);
+}
+
+int process(int a, int b, int(* func)(int, int)) {
+    return func(a, b);
+}
+
+int main() {
+    int a = 10, b = 20;
+    int res1 = process(a, b, add);
+    int res2 = process(a, b, minu);
+    int res3 = process(a, b, multi);
+    int res4 = process(a, b, divide);
+    cout << res1 << res2 << res3 << res4 << endl;
+    getchar();
+}
